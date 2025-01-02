f4297067a749b9c75688b018271350ff43d4de93
moronenfrans <frans.moronen@gmail.com>
2025-01-02 15:04:50 +0200

    1.2 tehty

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -11,6 +11,23 @@ const Header = (props) => {
 
 const Content = (props) => {
   console.log(props)
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  return(
+    <div>
+      <Part part1 = {part1} exercises1 = {exercises1}/>
+      <Part part2 = {part2} exercises2 = {exercises2}/>
+      <Part part3 = {part3} exercises3 = {exercises3}/>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  console.log(props)
   return(
     <div>
       <p>{props.part1} {props.exercises1}</p>
@@ -32,18 +49,15 @@ const Total = (props) => {
 const App = () => {
   console.log('Appin 1. tulostus')
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
   const exercises3 = 14
   const yht = 'Number of exercises'
 
   return (
     <div>
       <Header course = {course} />
-      <Content part1 = {part1} exercises1 = {exercises1} part2 = {part2} exercises2 = {exercises2} part3 = {part3} exercises3 = {exercises3}/>
+      <Content />
       <Total yht = {yht} total = {exercises1 + exercises2 + exercises3} />
     </div>
   )
